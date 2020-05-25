bd834239d97d319df7132213cf309cb77685bfbe
Faisal Nawaz <nawazfaisal953@gmail.com>
2020-05-26 00:38:17 +0500

    Add files via upload

---
+++ src/Appointment.java
@@ -0,0 +1,521 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+public class Appointment extends javax.swing.JFrame {
+
+    /**
+     * Creates new form Appointment
+     */
+    public Appointment() {
+        initComponents();
+        connect();
+        autoid();
+        LoadDoctor();
+        LoadPatient();
+        appointment_tblview();
+    }
+    
+     Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
+    String apno;
+    
+    public class Doctor{
+        String id;
+        String name;
+        public Doctor(String id,String name){
+            this.id=id;
+            this.name=name;
+        }
+        
+        public String toString(){
+            return name;
+        }
+           }
+    
+    public void LoadDoctor(){
+        try {
+            pst=con.prepareStatement("Select * from doctor");
+            rs=pst.executeQuery();
+            dname.removeAll();
+            
+            while(rs.next()){
+                
+                dname.addItem(new Doctor(rs.getString(1),rs.getString(2)));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Appointment.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    public class Patient{
+        String id;
+        String name;
+        public Patient(String id,String name){
+            this.id=id;
+            this.name=name;
+        }
+        
+        public String toString(){
+            return name;
+        }
+           }
+    
+    public void LoadPatient(){
+        try {
+            pst=con.prepareStatement("Select * from patient");
+            rs=pst.executeQuery();
+            pname.removeAll();
+            
+            while(rs.next()){
+                
+                pname.addItem(new Patient(rs.getString(1),rs.getString(2)));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Appointment.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+    
+    public void connect(){
+    
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_hospital?useTimezone=true&serverTimezone=UTC","root","");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void autoid(){
+    
+        try {
+            Statement s=con.createStatement();
+            rs=s.executeQuery("Select MAX(appointno) from appointment");
+            rs.next();
+            rs.getString("MAX(appointno)");
+            
+            if(rs.getString("MAX(appointno)")==null){
+                ano.setText("AP001");
+            }
+            else{
+           long id=Long.parseLong(rs.getString("MAX(appointno)").substring(2,rs.getString("MAX(appointno)").length()));
+           
+           id++;
+           ano.setText("AP"+String.format("%03d", id));
+           
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+     public void appointment_tblview(){
+        
+        try {
+            pst=con.prepareStatement("Select * from appointment");
+           
+            rs=pst.executeQuery();
+            
+            ResultSetMetaData Rsm=rs.getMetaData();
+            int count;
+            count=Rsm.getColumnCount();
+            
+            DefaultTableModel df=(DefaultTableModel)jTable1.getModel();
+            df.setRowCount(0);
+            
+            while(rs.next()){
+                Vector v=new Vector();
+                for(int i=1;i<=count;i++){
+                    v.add(rs.getString("appointno"));
+                    v.add(rs.getString("dname"));
+                    v.add(rs.getString("pname"));
+                    v.add(rs.getString("roomno"));
+                    v.add(rs.getString("date"));
+                    
+                }
+                df.addRow(v);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     }
+    
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        ano = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        aroom = new javax.swing.JSpinner();
+        adate = new com.toedter.calendar.JDateChooser();
+        jButton2 = new javax.swing.JButton();
+        pname = new javax.swing.JComboBox();
+        dname = new javax.swing.JComboBox();
+        jButton3 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel7 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setLayout(null);
+
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        jLabel1.setText("Appointment Date");
+        jLabel1.setOpaque(true);
+        jPanel3.add(jLabel1);
+        jLabel1.setBounds(20, 240, 100, 20);
+
+        ano.setBackground(new java.awt.Color(255, 255, 255));
+        ano.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        ano.setOpaque(true);
+        jPanel3.add(ano);
+        ano.setBounds(140, 40, 130, 20);
+
+        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        jLabel3.setText("Doctor Name");
+        jLabel3.setOpaque(true);
+        jPanel3.add(jLabel3);
+        jLabel3.setBounds(20, 90, 100, 20);
+
+        jLabel4.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        jLabel4.setText("Patient Name");
+        jLabel4.setOpaque(true);
+        jPanel3.add(jLabel4);
+        jLabel4.setBounds(20, 140, 100, 20);
+
+        jLabel5.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        jLabel5.setText("Room no");
+        jLabel5.setOpaque(true);
+        jPanel3.add(jLabel5);
+        jLabel5.setBounds(20, 190, 100, 20);
+
+        jLabel6.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        jLabel6.setText("Appointment no");
+        jLabel6.setOpaque(true);
+        jPanel3.add(jLabel6);
+        jLabel6.setBounds(20, 40, 100, 20);
+        jPanel3.add(aroom);
+        aroom.setBounds(140, 180, 80, 30);
+        jPanel3.add(adate);
+        adate.setBounds(140, 230, 140, 30);
+
+        jButton2.setBackground(new java.awt.Color(0, 102, 0));
+        jButton2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("Create");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel3.add(jButton2);
+        jButton2.setBounds(50, 280, 80, 30);
+
+        jPanel3.add(pname);
+        pname.setBounds(140, 130, 140, 30);
+
+        jPanel3.add(dname);
+        dname.setBounds(140, 80, 140, 30);
+
+        jButton3.setBackground(new java.awt.Color(153, 0, 0));
+        jButton3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("Exit");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel3.add(jButton3);
+        jButton3.setBounds(250, 280, 70, 30);
+
+        jButton1.setBackground(new java.awt.Color(51, 51, 255));
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Cancel");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel3.add(jButton1);
+        jButton1.setBounds(150, 280, 90, 30);
+
+        jTable1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        jTable1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Appointment no", "Doctor Name", "Patient Name", "Room no", "Appointment Date"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
+        jLabel7.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(0, 102, 102));
+        jLabel7.setText("   Appointment Registration");
+        jLabel7.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 102, 102), 2, true));
+        jLabel7.setOpaque(true);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(330, 330, 330)
+                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(10, 10, 10)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 490, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(24, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(58, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+         
+        String anum=ano.getText();
+        Doctor dt=(Doctor)dname.getSelectedItem();
+        Patient pt=(Patient)pname.getSelectedItem();
+        String room=aroom.getValue().toString();
+        
+        SimpleDateFormat dformat=new SimpleDateFormat("yyyy-MM-dd");
+        String date=dformat.format(adate.getDate());
+        
+        
+        
+        
+         try {
+  pst=con.prepareStatement("insert into appointment(appointno,dname,pname,roomno,date)Values(?,?,?,?,?)");
+  pst.setString(1,anum);
+  pst.setString(2,dt.id);
+  pst.setString(3,pt.id);
+  pst.setString(4,room);
+  pst.setString(5,date);
+  
+  
+  pst.executeUpdate();
+  
+  JOptionPane.showMessageDialog(this, "Channel Created");
+  
+  autoid();
+  ano.setText("");
+  dname.setSelectedIndex(-1);
+  
+  pname.setSelectedIndex(-1);
+  
+  aroom.setValue(0);
+  appointment_tblview();
+ 
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        // TODO add your handling code here:
+        DefaultTableModel d1=(DefaultTableModel)jTable1.getModel();
+        int selectindex=jTable1.getSelectedRow();
+        
+        apno=d1.getValueAt(selectindex, 0).toString();
+    }//GEN-LAST:event_jTable1MouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+        
+          
+        
+        
+        
+        
+         try {
+  pst=con.prepareStatement("delete from appointment where appointno=?");
+  pst.setString(1,apno);
+  
+  
+  
+  pst.executeUpdate();
+  
+  JOptionPane.showMessageDialog(this, "Appointment Cancelled");
+  
+  autoid();
+  ano.setText("");
+  dname.setSelectedIndex(-1);
+  
+  pname.setSelectedIndex(-1);
+  
+  aroom.setValue(0);
+  appointment_tblview();
+ 
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Appointment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Appointment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Appointment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Appointment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Appointment().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.toedter.calendar.JDateChooser adate;
+    private javax.swing.JLabel ano;
+    private javax.swing.JSpinner aroom;
+    private javax.swing.JComboBox dname;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JComboBox pname;
+    // End of variables declaration//GEN-END:variables
+}
